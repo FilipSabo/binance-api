0a73b9663bf4395422a9f7207e91baf8baa12c2e
Filip Sabo <filip.sabo95@gmail.com>
2024-01-10 12:56:34 +0100

    Pridanie chybovej hlasky

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ const App = () => {
 
   const [price, setPrice] = useState("")
   const [input, setInput] = useState("btcusdt")
+  // const [error, setError] = useState(false)
 
   const inputHolder = (event) => {
     if(event.key === "Enter"){
@@ -14,21 +15,31 @@ const App = () => {
 
   const url = "https://api.binance.com/api/v3/ticker/price?symbol="
   const searchingCrypto = url + input.toUpperCase()
-  const getData = async () => {
-    const response = await fetch(searchingCrypto)
-    const data = await response.json()
-    setPrice(`${data["symbol"]} : ${data["price"]}`)
+  const getFetch = () => {
+    fetch(searchingCrypto)
+    .then (res => {
+      return res.json()
+    })
+    .then (data => {
+      setPrice(`${data["symbol"]} : ${data["price"]}`)
+    })
+    .catch ( err => {
+      if (err.message === "Failed to fetch"){
+        setPrice("Zla hodnota, prosim zadat znova")
+      }
+    })
   }
   useEffect( () => {
-      getData()
+      getFetch()
   })
 
-  return <div className='search'>
-
-      <h1>Crypto Search</h1>
-      <input type="text" className="input" placeholder='Hladany symbol' onKeyDown={(event) => inputHolder(event)} />
-      <h1>{price}</h1>
+  return <section>
+    <div className='search'>
+        <h1>Crypto Search</h1>
+        <input type="text" className="input" placeholder='Hladany symbol' onKeyDown={(event) => inputHolder(event)} />
+        <h1>{price}</h1>
   </div>
+</section>
 }
 
 export default App
